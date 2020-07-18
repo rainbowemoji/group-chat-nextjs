74376a1665390f50a692611b8801123a314c2ba5
rainbowemoji <heroskyweb@gmail.com>
2020-07-18 15:10:26 -0500

    Reworked sample to suit my needs

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,37 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>House of Asdell-LeBlanc</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className="title">House of Asdell-LeBlanc</h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Welcome to our house! Please enter a name to get started.
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <form>
+          <p>
+            <input name="name" placeholder="your name here..." />
+          </p>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <p>
+            <label><input name="agree" type="checkbox" /> I agree to the <a href="https://firstdonoharm.dev/version/2/1/license.html">Hippocratic License</a>.</label>
+          </p>
 
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <p>
+            <button>Enter</button>
+          </p>
+        </form>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+        🐻🐻
       </footer>
 
       <style jsx>{`
@@ -126,6 +101,11 @@ export default function Home() {
 
         .description {
           line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        input, button {
+          line-height 1.5;
           font-size: 1.5rem;
         }
 
@@ -205,5 +185,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = dev ? 3000 : 80;
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log("> Ready on http://localhost:" + port);
+  });
+});
