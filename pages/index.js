691b85bd172c0a22d93086f9c661f51d85f22a1b
rainbowemoji <heroskyweb@gmail.com>
2020-07-18 15:46:58 -0500

    Added an adorable photo

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,10 @@ export default function Home() {
       </Head>
 
       <main>
+        <div className="card">
+          <img alt="A photo of Joe LeBlanc and Andrew Asdell in adorable suits." src="andrew-and-joe.jpg" height="390" width="400" />
+        </div>
+
         <h1 className="title">House of Asdell-LeBlanc</h1>
 
         <p className="description">
