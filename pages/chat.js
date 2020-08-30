68e4da120a1214f26176655ac518c5b5f920cb86
rainbowemoji <heroskyweb@gmail.com>
2020-08-30 13:47:47 -0500

    Stripping down this CSS, don't need it

--- pages/chat.js
+++ pages/chat.js
@@ -19,21 +19,6 @@ export default function Chat({ subject }) {
         </div>
       ))}
       <input name="chatterbox" placeholder="type your message here..." />
-      <style jsx>
-        {`
-          .card {
-            margin: 1rem;
-            flex-basis: 45%;
-            padding: 1.5rem;
-            text-align: left;
-            color: inherit;
-            text-decoration: none;
-            border: 1px solid #eaeaea;
-            border-radius: 10px;
-            transition: color 0.15s ease, border-color 0.15s ease;
-          }
-        `}
-      </style>
     </div>
   );
 }
